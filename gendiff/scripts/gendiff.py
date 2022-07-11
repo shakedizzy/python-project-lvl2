5b95740092047bad05f525a19acffd4f0658bb97
shakedizzy <shakedizzy81@gmail.com>
2022-07-11 22:28:22 +0400

    add help to startup script

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,8 +1,23 @@
 #!/usr/bin/env python3
 
+import argparse
+import gendiff.engine
+
+
+def define_args():
+    parser = argparse.ArgumentParser(description='Compares two configuration \
+        files and shows a difference.')
+
+    parser.add_argument('first_file')
+    parser.add_argument('second_file')
+
+    args = parser.parse_args()
+    return args
+
 
 def main():
-    return
+    args = define_args()
+    gendiff.engine.main(args)
 
 
 if __name__ == '__main__':
