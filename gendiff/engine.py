9d1a0a4ffbe4dc41b9a93a49909f28928400c1e0
shakedizzy <shakedizzy81@gmail.com>
2022-07-16 13:31:56 +0400

    Add docstring to generate_diff

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,4 +1,10 @@
 def generate_diff(args):
+    """Display DIFF between two JSON files.
+
+    Keyword arguments:
+    filename1, filename2 — files to compare (Required)
+    -f --format FORMAT — output format (json by default)
+    """
     print('Hooray!')
 
 
