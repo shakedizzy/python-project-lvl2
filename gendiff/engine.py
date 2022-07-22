e0444951c12ede953d3f641b954252d9bd630b80
shakedizzy <shakedizzy81@gmail.com>
2022-07-22 18:19:03 +0400

    Make first working version of project
    This is first working version of app that compares two json files and prints
    difference in them

--- gendiff/engine.py
+++ gendiff/engine.py
@@ -1,15 +1,65 @@
-def generate_diff(args):
+import os
+import json
+
+
+def read_files(file_1, file_2):
+    print(file_1, file_2)
+
+    with open(os.path.abspath(file_1), 'r') as jsonFile_1, \
+         open(os.path.abspath(file_2), 'r') as jsonFile_2:
+        return json.load(jsonFile_1), json.load(jsonFile_2)
+
+
+def gen_unique_keys(keys_1, keys_2):
+    """Generate list of unique keys for both files"""
+
+    unique_keys = []
+    for key in keys_1:
+        if key not in unique_keys:
+            unique_keys.append(key)
+        for key in keys_2:
+            if key not in unique_keys:
+                unique_keys.append(key)
+    print(sorted(unique_keys))
+    return sorted(unique_keys)
+
+
+def generate_diff(file_1, file_2, format):
     """Display DIFF between two JSON files.
 
-    Keyword arguments:
-    filename1, filename2 — files to compare (Required)
-    -f --format FORMAT — output format (json by default)
+    Positional arguments:
+    file_1, file2 - files to compare (Required)
+    Optional arguments:
+    format - output format (json by default)
     """
-    print('Hooray!')
+    object_1, object_2 = read_files(file_1, file_2)
+
+#   Spaghetti code. Needs refactoring
+    keys_1 = list(object_1.keys())
+    keys_2 = list(object_2.keys())
+    keys_list = gen_unique_keys(keys_1, keys_2)
+
+    output_string = '''{\n'''
+
+    for c_key in keys_list:
+        if c_key in object_1.keys() and c_key in object_2.keys():
+            if object_1[c_key] == object_2[c_key]:
+                output_string += f'''    {c_key}: {object_1[c_key]}\n'''
+            else:
+                output_string += f'''  - {c_key}: {object_1[c_key]}\n'''
+                output_string += f'''  + {c_key}: {object_2[c_key]}\n'''
+        elif c_key in object_1.keys():
+            output_string += f'''  - {c_key}: {object_1[c_key]}\n'''
+        else:
+            output_string += f'''  + {c_key}: {object_2[c_key]}\n'''
+
+    output_string += '''}'''
+
+    print(output_string)
 
 
-def main(args):
-    generate_diff(args)
+def main(file_1, file_2, format):
+    generate_diff(file_1, file_2, format)
 
 
 if __name__ == '__main__':
