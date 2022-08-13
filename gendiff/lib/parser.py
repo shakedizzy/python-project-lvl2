a44056e1884c0f7ac29f4982045a22f3cd2181e6
shakedizzy <shakedizzy81@gmail.com>
2022-08-13 16:37:53 +0400

    Move file parser to external module

--- gendiff/lib/parser.py
+++ gendiff/lib/parser.py
@@ -6,11 +6,8 @@ import yaml
 def read_file(filename):
     _, extension = os.path.splitext(filename)
     input_file = open(os.path.abspath(filename), 'r')
-    return parse_file(input_file, extension)
 
-
-def parse_file(input_file, extension):
-    if extension == 'json':
+    if extension == '.json':
         return json.load(input_file)
-    elif extension == 'yml' or extension == 'yaml':
+    elif extension == '.yml' or extension == '.yaml':
         return yaml.safe_load(input_file)
